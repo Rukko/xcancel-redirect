6d4bac72ef8618ee1ccef0672d8f168bed12bff2
Rukko <jonydiz@gmail.com>
2026-01-28 18:03:38 +0100

    Add meta information for xcancel-redirect userscript

---
+++ xcancel-redirect.meta.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         X → Twitter Frontend Redirect
+// @namespace    http://rukko.es
+// @version      5.0.0
+// @license      MIT
+// @description  Redirects x.com to your preferred Twitter frontend. Choose from xcancel, nitter instances, and more.
+// @author       Rukko
+// @match        https://x.com/*
+// @match        https://twitter.com/*
+// @match        https://xcancel.com/*
+// @match        https://*.nitter.net/*
+// @match        https://nitter.poast.org/*
+// @match        https://nitter.privacydev.net/*
+// @match        https://nitter.1d4.us/*
+// @match        https://nitter.unixfox.eu/*
+// @match        https://nitter.hu/*
+// @match        https://bird.makeup/*
+// @match        https://twstalker.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=x.com
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @downloadURL  https://raw.githubusercontent.com/Rukko/xcancel-redirect/main/xcancel-redirect.user.js
+// @updateURL    https://raw.githubusercontent.com/Rukko/xcancel-redirect/main/xcancel-redirect.meta.js
+// ==/UserScript==
